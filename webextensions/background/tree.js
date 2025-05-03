ef4a05ee4a5d6021e02abb6c54d94426f5fdd598
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2025-05-04 05:42:27 +0900

    Don't set invalid destination window id

--- webextensions/background/tree.js
+++ webextensions/background/tree.js
@@ -14,7 +14,7 @@
  * The Original Code is the Tree Style Tab.
  *
  * The Initial Developer of the Original Code is YUKI "Piro" Hiroshi.
- * Portions created by the Initial Developer are Copyright (C) 2011-2024
+ * Portions created by the Initial Developer are Copyright (C) 2011-2025
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s): YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
@@ -1461,8 +1461,9 @@ export async function moveTabs(tabs, options = {}) {
   let newWindow = options.destinationPromisedNewWindow;
 
   let destinationWindowId = options.destinationWindowId;
-  if (!destinationWindowId && !newWindow)
-    destinationWindowId = TabsStore.getCurrentWindowId();
+  if (!destinationWindowId && !newWindow) {
+    destinationWindowId = TabsStore.getCurrentWindowId() || windowId;
+  }
 
   const isAcrossWindows = windowId != destinationWindowId || !!newWindow;
 
